3947873edc06532367d8bceebfc52a522c2ed6ab
Kyle Johnson <ktjohnson@student.fullsail.edu>
2020-02-13 10:44:05 -0500

    finished up javascript file

--- mynode.js
+++ mynode.js
@@ -1,50 +1,33 @@
+"use strict";
+//The rl.question() method displays the query by writing it to the output, waits for user input to be provided on input, then invokes the callback function passing the provided input as the first argument.
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+//Create methods to output a valid grade
+function letter(num){
+  var letterGrade = "";
+    if ((num <= 110) && (num >= 90)){
+      letterGrade = "A";
+    }
+    else if ((num <= 89.99) && (num >= 80)){
+      letterGrade = "B";
+    }
+    else if ((num <= 79.99) && (num >= 70)){
+      letterGrade = "C";
+    }
+    else if ((num <= 69.99) && (num >= 60)){
+      letterGrade = "D";
+    }
+    else if((num <= 59.99) && (num >= 0)){
+      letterGrade = "F";
+    }
+    return letterGrade;
+  }
 
-var grader = "Kyle Johnson"
-console.log("Hi, welcome to " + grader + "'s grader.")
-
-rl.question('What is your Name? ', function (name) {
-  // TODO: Log the answer in a database
-  console.log("Thank you:", name);
-  rl.question("What is the name of your assignment? ", function (assignment) {
-    // TODO: Log the answer in a database
-    rl.question("What was the grade that you received on the " + assignment + " assignment.", function (grade) {
-      // TODO: Log the answer in a database
-        rl.close();
-    })
-  })
-})
-
-var gradeFloat = parseFloat(grade);
-
-var letterGrade = "";
-
-switch (gradeFloat){
-  case (gradefloat <= 110 && gradefloat >= 90):
-  letterGrade = "A";
-  break;
-  case (gradefloat <= 89.99 && gradefloat >= 80):
-  letterGrade = "B";
-  break;
-  case (gradefloat <= 79.99 && gradefloat >= 70):
-  letterGrade = "C";
-  break;
-  case (gradefloat <= 69.99 && gradefloat >= 60):
-  letterGrade = "D";
-  break;
-  case (gradefloat <= 59.99 && gradefloat >= 0):
-  letterGrade = "F";
-  break;
-  default:
-  alert("none");
-  break;
-}
-
+//Create a class "Grader"
 class Grader {
   constructor(mName, mAssignment, mGrade, mLetterGrade){
     this.name = mName;
@@ -57,5 +40,19 @@ class Grader {
   }
 }
 
-var g1 = new Grader(name, assignment, grade, letterGrade);
-g1.display();
+//Create a grader
+var grader = "Kyle Johnson"
+console.log("Hi, welcome to " + grader + "'s grader.")
+//Using the command line ask user a question for students name, assignment name, and grade
+rl.question('What is your Name? ',(name)=>{
+  console.log("Thank you:", name);
+  rl.question("What is the name of your assignment? ",  (assignment)=>{
+    rl.question("What was the grade that you received on the " + assignment + " assignment.", (grade) => {
+      var gradeFloat = Number.parseFloat(grade);
+      var letterGrade = letter(gradeFloat);
+      var g1 = new Grader(name, assignment, grade, letterGrade);
+      g1.display();
+      rl.close();
+    })
+  })
+})
