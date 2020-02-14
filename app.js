3ce916fd1387978e122b4defc991386973edfdaf
Kyle Johnson <ktjohnson@student.fullsail.edu>
2020-02-14 09:02:12 -0500

    finished up second assignment

---
+++ app.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+var http = require("http");
+var path = require("path");
+var url = require("url");
+
+http.createServer(function (req, res){
+  var parsed = url.parse(req.url);
+  var filename = path.parse(parsed.pathname);
+
+  var filen = "";
+  if(filename.name == ""){
+    filen = "index";
+  }else{
+    filen = filename.name;
+  }
+
+
+  fs.readFile(filen + ".html", function(err,data){
+
+
+
+    res.writeHead(200);
+    res.write("<script> var highlighted = 'magenta';</script>")
+    
+    res.end(data);
+  })
+
+}).listen("8080")
